0c81b0f6639398c7baa63316febd64f05a198185
Rita Kajimura <56577971+kajirita2002@users.noreply.github.com>
2020-12-04 21:43:57 +0900

    Update config.tf

--- ecr/config.tf
+++ ecr/config.tf
@@ -1,6 +1,6 @@
 terraform {
     backend "s3" {
-        bucket = "container-era-terraform-sample1" # ここの名前は各自で考えましょう
+        bucket = "container-era-terraform-sample1" 
         key = "terraform-sample/ecr/terraform.tfstate"
         region = "ap-northeast-1"
         profile = "sample3"
